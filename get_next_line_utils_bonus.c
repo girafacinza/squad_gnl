42a1713d25dc647dd79bacf83e89f95e7e99e997
Lucas Gallo <lucasagallo@gmail.com>
2020-05-16 19:46:40 -0300

    working with bonus on 42TESTERS_GNL

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 void			ft_bzero(void *b, size_t len)
 {
